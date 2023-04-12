035eba8bffbd784288ed357cf7d7876ea64336b1
Urmzd Mukhammadnaim <hello@urmzd.com>
2023-04-12 04:49:24 -0300

    fix: ensure secret is retrieved correctly

--- cmd/control/main.go
+++ cmd/control/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	pkg "github.com/urmzd/sunrise-lamp/pkg"
@@ -15,6 +17,57 @@ import (
 type ControlEvent struct {
 	Name  string `json:"name"`
 	Level int    `json:"level"`
+}
+
+func handler(ctx context.Context, event ControlEvent) error {
+	log.Info().Str("Name", event.Name).Int("Level", event.Level).Msg("Received control event")
+
+	deviceName, err := getDeviceName(ctx, event.Name)
+	if err != nil {
+		return err
+	}
+
+	credsArn := os.Getenv("CREDS")
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to load default config")
+		return err
+	}
+
+	client := secretsmanager.NewFromConfig(cfg)
+	resp, err := client.GetSecretValue(ctx, &secretsmanager.GetSecretValueInput{
+		SecretId: &credsArn,
+	})
+
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to retrieve secret")
+		return err
+	}
+
+	var creds map[string]string
+	err = json.Unmarshal([]byte(*resp.SecretString), &creds)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to unmarshal secret")
+		return err
+	}
+
+	pkg.MqttServer = os.Getenv("SERVER")
+	pkg.MqttUser = creds["user"]
+	pkg.MqttPassword = creds["password"]
+	pkg.DeviceFriendlyName = deviceName
+
+	log.Info().Str("MqttServer", pkg.MqttServer).Str("MqttUser", pkg.MqttUser).Str("DeviceFriendlyName", pkg.DeviceFriendlyName).Msg("Environment variables set")
+
+	mqttClient := pkg.NewClient(pkg.MqttServer, pkg.MqttUser, pkg.MqttPassword)
+	currentBrightness := pkg.GetCurrentBrightness(mqttClient, pkg.DeviceFriendlyName)
+
+	log.Info().Int("CurrentBrightness", currentBrightness).Msg("Current brightness retrieved")
+
+	pkg.SetBrightness(mqttClient, pkg.DeviceFriendlyName, currentBrightness+1)
+
+	log.Info().Msg("Brightness updated successfully")
+
+	return nil
 }
 
 func main() {
@@ -42,31 +95,4 @@ func getDeviceName(ctx context.Context, name string) (string, error) {
 
 	log.Info().Str("DeviceName", deviceMapping.DeviceName).Msg("Device name retrieved")
 	return deviceMapping.DeviceName, nil
-}
-
-func handler(ctx context.Context, event ControlEvent) error {
-	log.Info().Str("Name", event.Name).Int("Level", event.Level).Msg("Received control event")
-
-	deviceName, err := getDeviceName(ctx, event.Name)
-	if err != nil {
-		return err
-	}
-
-	pkg.MqttServer = os.Getenv("SERVER")
-	pkg.MqttUser = os.Getenv("USER")
-	pkg.MqttPassword = os.Getenv("PASSWORD")
-	pkg.DeviceFriendlyName = deviceName
-
-	log.Info().Str("MqttServer", pkg.MqttServer).Str("MqttUser", pkg.MqttUser).Str("DeviceFriendlyName", pkg.DeviceFriendlyName).Msg("Environment variables set")
-
-	client := pkg.NewClient(pkg.MqttServer, pkg.MqttUser, pkg.MqttPassword)
-	currentBrightness := pkg.GetCurrentBrightness(client, pkg.DeviceFriendlyName)
-
-	log.Info().Int("CurrentBrightness", currentBrightness).Msg("Current brightness retrieved")
-
-	pkg.SetBrightness(client, pkg.DeviceFriendlyName, currentBrightness+1)
-
-	log.Info().Msg("Brightness updated successfully")
-
-	return nil
 }
